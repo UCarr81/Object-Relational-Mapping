c68b3414508d0c2289535281abf0cb80fb9c47ec
Uriel <CarrilloUriel81@gmail.com>
2023-12-11 18:59:36 -0800

    Category, Products, products tags, tags all have unique ID and Values

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,40 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    prodcut_name: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          isDecimal: true,
+        },
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValie: 10,
+        validate: {
+          isNumeric: true,
+        },
+      },
+      catergory_id: {
+        type: DataTypes.INTEGER,
+        refrebces: {
+          model: 'category',
+          key: 'id',
+        },
+      },
+    },
   },
   {
     sequelize,
