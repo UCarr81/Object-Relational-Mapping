cd41115944b465e056267103a3d0e7f6a993e03c
Uriel <CarrilloUriel81@gmail.com>
2023-12-14 22:04:14 -0800

    Fixed Code Everthing works as it should, put method and delete method have been updated

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -53,102 +53,88 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
+// create new product
 router.post('/', async (req, res) => {
   try {
     const product = await Product.create(req.body);
-    
+
     if (req.body.tagIds && req.body.tagIds.length) {
       const productTagIdArr = req.body.tagIds.map((tag_id) => ({
-        product_id: product_id,
+        product_id: product.id,
         tag_id,
       }));
       await ProductTag.bulkCreate(productTagIdArr);
     }
+
     res.status(201).json(product);
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
   }
-
-
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
 });
+
 
 // update product
-router.put('/:id', (req, res) => {
-  // update product data
-  Product.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((product) => {
-      if (req.body.tagIds && req.body.tagIds.length) {
-        
-        ProductTag.findAll({
-          where: { product_id: req.params.id }
-        }).then((productTags) => {
-          // create filtered list of new tag_ids
-          const productTagIds = productTags.map(({ tag_id }) => tag_id);
-          const newProductTags = req.body.tagIds
-          .filter((tag_id) => !productTagIds.includes(tag_id))
-          .map((tag_id) => {
-            return {
-              product_id: req.params.id,
-              tag_id,
-            };
-          });
+// Assuming you're using Sequelize for database operations
 
-            // figure out which ones to remove
-          const productTagsToRemove = productTags
-          .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-          .map(({ id }) => id);
-                  // run both actions
-          return Promise.all([
-            ProductTag.destroy({ where: { id: productTagsToRemove } }),
-            ProductTag.bulkCreate(newProductTags),
-          ]);
-        });
-      }
+router.put('/:product_id/tags', async (req, res) => {
+  try {
+    const productId = req.params.product_id;
+    const tagIds = req.body.tags;
 
-      return res.json(product);
-    })
-    .catch((err) => {
-      // console.log(err);
-      res.status(400).json(err);
+    // Check if the product exists
+    const existingProduct = await Product.findByPk(productId);
+    if (!existingProduct) {
+      res.status(404).json({ message: 'No product found with this id' });
+      return;
+    }
+
+    // Remove existing tags for the product
+    await ProductTag.destroy({
+      where: {
+        product_id: productId,
+      },
     });
+
+    // Add new tags for the product
+    await ProductTag.bulkCreate(tagIds.map(tagId => ({
+      product_id: productId,
+      tag_id: tagId,
+    })));
+
+    // Respond with the updated product (including tags)
+    const updatedProduct = await Product.findByPk(productId, {
+      include: [
+        {
+          model: Tag,
+          through: ProductTag,
+        },
+      ],
+    });
+
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
+
+// delete product
 router.delete('/:id', async (req, res) => {
   try {
-    const product = await Product.destroy({
+    const deletedProductCount = await Product.destroy({
       where: {
         id: req.params.id,
       },
     });
-    if (product > 0) {
-      res.json({ message: 'No product found with this id!' });
-    } else {
+
+    if (deletedProductCount > 0) {
+      // Product was deleted
       res.status(200).json({ message: 'Product deleted!' });
+    } else {
+      // No product found with the given id
+      res.status(404).json({ message: 'No product found with this id!' });
     }
   } catch (err) {
     console.error(err);
